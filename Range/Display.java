09c93cbeb2868d81d4094db0ed4cc7e03fc3bbaa
Pakshal <pakshal1256@gmail.com>
2022-11-04 17:58:16 +0530

    commit

--- Range/Display.java
+++ Range/Display.java
@@ -5,6 +5,11 @@ class Display
 {
     public static void main(String args[])
     {
+        int No = 0;
+        int No2 = 0;
+
         System.out.println("Jay Ganesh....");
+
+        Scanner sobj = new Scanner(System.in);
     }
 }
