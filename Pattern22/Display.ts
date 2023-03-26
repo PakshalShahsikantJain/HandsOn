44bd9c6b11fe187d41f2b8e23fb721465c9e2a9d
Pakshal <pakshal1256@gmail.com>
2023-03-26 19:06:19 +0530

    commit

---
+++ Pattern22/Display.ts
@@ -0,0 +1,52 @@
+/*
+    Author : Pakshal Shashikant Jain 
+    Date : 22/03/2023
+    Program  : To Print Below Given Pattern 
+    
+    Input : iRow = 4 iCol = 4
+    Output : * # # #
+             * * # #
+             * * * #
+             * * * * 
+*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Function To Print Pattern
+function Pattern(No : number,No2 : number) 
+{
+    var i : number = 0;
+    var j : number = 0;
+
+    //Logic To print Pattern
+    console.log("Printing Given Pattern : ");
+    for(i = 0;i < No;i++) 
+    {
+        for(j = 0;j < No2;j++) 
+        {
+            if((i == j)||(i > j)) 
+            {
+                process.stdout.write("*\t");    
+            }
+            else 
+            {
+                process.stdout.write("#\t");
+            }
+        }
+        console.log();
+    }
+}
+
+//Main Function
+function main() 
+{
+    var No : number = 4;
+    var No2 : number = 4;
+
+    console.log("Jay Ganesh....");
+
+    //Call To Pattern Function
+    Pattern(No,No2);
+}
+
+main();
