249f926672672f5de05537658201a9d9ec8636a1
Pakshal <pakshal1256@gmail.com>
2023-03-30 20:33:26 +0530

    commit

--- Pattern24/Display.js
+++ Pattern24/Display.js
@@ -1,6 +1,9 @@
+//Function To Display Pattern
 function Display(No, No2) {
     var i = 0;
     var j = 0;
+    //Logic To Print Pattern 
+    console.log("Printing Below Given Pattern...");
     for (i = 1; i <= No; i++) {
         for (j = 1; j <= No2; j++) {
             if (i == j) {
@@ -13,10 +16,12 @@ function Display(No, No2) {
         console.log();
     }
 }
+//Main Function
 function main() {
-    var No = 4;
-    var No2 = 4;
+    var No = 5;
+    var No2 = 5;
     console.log("Jay Ganesh....");
+    //Call To Display Function
     Display(No, No2);
 }
 main();
--- Pattern24/Display.ts
+++ Pattern24/Display.ts
@@ -1,8 +1,11 @@
+//Function To Display Pattern
 function Display(No : number,No2 : number) 
 {
     var i : number = 0;
     var j : number = 0;
 
+    //Logic To Print Pattern 
+    console.log("Printing Below Given Pattern...");
     for(i = 1;i <= No;i++)
     {
         for(j = 1;j <= No2;j++)
@@ -20,13 +23,15 @@ function Display(No : number,No2 : number)
     }
 }
 
+//Main Function
 function main() 
 {
-    var No : number = 4;
-    var No2 : number = 4;
+    var No : number = 5;
+    var No2 : number = 5;
 
     console.log("Jay Ganesh....");
 
+    //Call To Display Function
     Display(No,No2);
 }
 
