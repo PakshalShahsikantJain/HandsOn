18f3ef30de7d7f58cd0ce344882b24fbd880f138
Pakshal <pakshal1256@gmail.com>
2023-09-01 12:49:26 +0530

    commit

---
+++ Toggle_7_10_Bit/Modify.c
@@ -0,0 +1,85 @@
+/*
+    Author : Pakshal Shashikant Jain 
+    Date : 01/09/2023
+    Program : Write a program which accept one number from user and toggle 7th and
+              10th bit of that number. Return modified number. 
+*/
+
+//Required Libraries
+#include<stdio.h>
+
+typedef int unsigned UINT;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Function To Toggle Bit
+UINT ToggleBit(UINT iNo)
+{                              //    0    0    0    0    0    2    4    0
+    int iMask = 0X00000240;    //   0000 0000 0000 0000 0000 0010 0100 0000
+    int iResult = 0;
+    UINT iret = 0;
+
+    iResult = iNo ^ iMask;    
+
+    return iResult;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Main Function
+int main() 
+{
+    int iNo = 0;
+    UINT iret = 0;
+
+    printf("Jay Ganesh....\n");
+
+    //Taking Input from user
+    printf("Enter One Number \n");
+    scanf("%d",&iNo);
+
+    //Call To ToggleBit Function
+    iret = ToggleBit(iNo);
+
+    //Printing Modified Number
+    printf("Modified Number is : %u",iret);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+
+Decimal            Binary
+0                  0000
+1                  0001
+2                  0010
+3                  0011
+4                  0100
+5                  0101
+6                  0110
+7                  0111
+8                  1000
+9                  1001
+10                 1010
+11                 1011
+12                 1100
+13                 1101
+14                 1110
+15                 1111
+*/
---
+++ Toggle_7_10_Bit/Modify.java
@@ -0,0 +1,83 @@
+/*
+    Author : Pakshal Shashikant Jain 
+    Date : 01/09/2023
+    Program : Write a program which accept one number from user and toggle 7th and
+              10th bit of that number. Return modified number.  
+
+              Input : 137
+              Output : 713
+*/
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Required Libraries
+import java.lang.*;
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Class Declaration
+class Bit
+{
+    //Characteristics of Class
+    int No;
+
+    //Constructor of Class
+    Bit()
+    {
+        this.No = 0;
+    }
+
+    //Behaviour of Class To Take Input from user
+    void Accept()
+    {
+        //Creating object of Scanner Class To Take Input from user
+        Scanner sobj = new Scanner(System.in);
+
+        //Taking Input from user
+        System.out.println("Enter One Number");
+        this.No = sobj.nextInt();
+    }
+
+    //Behaviour of Class 
+    int Togglebit()
+    {
+        int iResult = 0;           //  0    0    0    0    0    2    4    0
+        int iMask = 0X00000240;   //  0000 0000 0000 0000 0000 0010 0100 0000
+
+        if(this.No < 0)
+        {
+            this.No = -this.No;
+        }
+
+        //Logic To Toggle 7 Bit
+        iResult = iMask ^ this.No;
+
+        return iResult;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Main Class
+class Modify
+{
+    //Main Function
+    public static void main(String args[])
+    {
+        int iret = 0;
+
+        System.out.println("Jay Ganesh....");
+
+        //Creating object of  Class
+        Bit bobj = new Bit();
+
+        //Call To Behaviour of Class
+        bobj.Accept();
+        iret = bobj.Togglebit();
+
+        //Printing Modified Number 
+        System.out.printf("Modified Number is : %d",iret);
+    }
+}
